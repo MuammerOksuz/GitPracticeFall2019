60557a9820d59f1e5920f4d1450ad895072d3716
“MuammerOksuz” <muammer@gmail.com>
2020-02-29 14:39:21 -0600

    for lop new

---
+++ src/day01/forloop.java
@@ -0,0 +1,8 @@
+package day01;
+public class forloop {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 9; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
