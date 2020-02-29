4ed83773df1a8b63094abcd14a6b6ec404195d10
“MuammerOksuz” <muammer@gmail.com>
2020-02-29 14:33:56 -0600

    2nd version

--- src/day01/StringPractice.java
+++ src/day01/StringPractice.java
@@ -3,5 +3,6 @@ package day01;
 public class StringPractice {
     public static void main(String[] args) {
         System.out.println("Name = " + "Muammer");
+        System.out.println("Hello World");
     }
 }
